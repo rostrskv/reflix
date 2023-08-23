ad11e8090215f546cc6149170bca7b2f68fc3596
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-23 13:11:20 +0300

    Fix multiple config API calls, refactoring in tmdb wrapper

--- src/services/movies.js
+++ src/services/movies.js
@@ -8,45 +8,56 @@ function getTmdbUrl(url) {
   return fullUrl;
 }
 
+async function callMovieApi(url) {
+  const fullUrl = getTmdbUrl(url);
+  const response = await fetch(fullUrl);
+  const result = await response.json();
+  return result;
+}
+
+/**
+ * Get tmdb configuration
+ * @example 
+   {
+    images: {
+      base_url: "http://image.tmdb.org/t/p/",
+      secure_base_url: "https://image.tmdb.org/t/p/",
+      backdrop_sizes: ["w300", "w780", "w1280", "original"],
+      logo_sizes: ["w45", "w92", "w154", "w185", "w300", "w500", "original"],
+      poster_sizes: [
+        "w92",
+        "w154",
+        "w185",
+        "w342",
+        "w500",
+        "w780",
+        "original",
+      ],
+      profile_sizes: ["w45", "w185", "h632", "original"],
+      still_sizes: ["w92", "w185", "w300", "original"],
+    },
+  }
+ */
 async function getConfig() {
-  const configUrl = getTmdbUrl("/3/configuration");
-  const config = await (await fetch(configUrl)).json();
-  // const config = {
-  //   images: {
-  //     base_url: "http://image.tmdb.org/t/p/",
-  //     secure_base_url: "https://image.tmdb.org/t/p/",
-  //     backdrop_sizes: ["w300", "w780", "w1280", "original"],
-  //     logo_sizes: ["w45", "w92", "w154", "w185", "w300", "w500", "original"],
-  //     poster_sizes: [
-  //       "w92",
-  //       "w154",
-  //       "w185",
-  //       "w342",
-  //       "w500",
-  //       "w780",
-  //       "original",
-  //     ],
-  //     profile_sizes: ["w45", "w185", "h632", "original"],
-  //     still_sizes: ["w92", "w185", "w300", "original"],
-  //   },
-  // };
-  return config;
+  return callMovieApi("/3/configuration");
 }
 
 let imageBaseUrl;
 async function getImageBaseUrl() {
-  return imageBaseUrl ?? (await getConfig()).images.secure_base_url;
+  return (
+    imageBaseUrl ?? (imageBaseUrl = (await getConfig()).images.secure_base_url)
+  );
 }
+
 /**
- * Get lists of posts from API
+ * Get lists of movies from API
  * @returns {Promise<{userId:number, id:number, title:string, overview:string, poster_path:string}[]>}
  */
-export async function getTrending() {
-  const trendingUrl = getTmdbUrl("/3/trending/movie/day");
-  const trending = await (await fetch(trendingUrl)).json();
+async function getMovieList(url) {
   const imageBaseUrl = await getImageBaseUrl();
+  const result = await callMovieApi(url);
 
-  const mappedTrending = trending.results
+  return result.results
     .slice(0, CONSTANTS.movieListLimit)
     .map(({ id, title, overview, poster_path }) => ({
       id,
@@ -54,35 +65,29 @@ export async function getTrending() {
       overview,
       poster_path: poster_path && `${imageBaseUrl}w185${poster_path}`,
     }));
+}
 
-  return mappedTrending;
+/**
+ * Get lists of trending movies from API
+ */
+export async function getTrending() {
+  return getMovieList("/3/trending/movie/day");
+}
+
+/**
+ * Get searched movies
+ */
+export async function searchMovie(query) {
+  return getMovieList(`/3/search/movie?query=${query}`);
 }
 
 export async function getMovieDetail(movieId) {
   const imageBaseUrl = await getImageBaseUrl();
-  const url = getTmdbUrl(`/3/movie/${movieId}`);
-  const response = await fetch(url);
-  const movie = await response.json();
+  const movie = await callMovieApi(`/3/movie/${movieId}`);
   return {
     id: movie.id,
     title: movie.title,
     overview: movie.overview,
     poster_path: `${imageBaseUrl}w500${movie.poster_path}`,
   };
-}
-
-export async function searchMovie(query) {
-  const imageBaseUrl = await getImageBaseUrl();
-  const url = getTmdbUrl(`/3/search/movie?query=${query}`);
-  const response = await fetch(url);
-  const searchResult = await response.json();
-
-  return searchResult.results
-    .slice(0, CONSTANTS.movieListLimit)
-    .map(({ id, title, overview, poster_path }) => ({
-      id,
-      title,
-      overview,
-      poster_path: poster_path &&  `${imageBaseUrl}w185${poster_path}`,
-    }));
 }
