c5a3eefc30f57ae52a849ffce3c9b690d6daa056
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-28 17:18:08 +0300

    Close modeal with clicking on background

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,8 +6,11 @@ import "bootstrap-icons/font/bootstrap-icons.css";
  */
 export default function Modal({ message, gifQuery, closeModal }) {
   return (
-    <div className="modal">
-      <div className="modal-content">
+    <div className="modal" onClick={closeModal}>
+      <div
+        className="modal-content"
+        onClick={(event) => event.stopPropagation()}
+      >
         <i class="close bi bi-x" onClick={closeModal} title="Close" />
         <div>{message}</div>
       </div>
